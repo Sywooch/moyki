29c7da1d0995938b31fa15890018eacca1cb57bc
Anatolik <woody_green@i.ua>
2015-09-27 11:25:26 +0300

    molotok

--- views/site/about.php
+++ views/site/about.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        hernja
+        mojki
     </p>
 
     <code><?= __FILE__ ?></code>
