6aeb9e6acd7d42fa3e6e844a701b2f364bee90e4
alex_azbukin <azbukin777@gmail.com>
2015-09-29 11:12:53 +0300

    hide index file from url

--- web/index.php
+++ web/index.php
@@ -10,18 +10,3 @@ require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 $config = require(__DIR__ . '/../config/web.php');
 
 (new yii\web\Application($config))->run();
-
-<?php
-
-class LoginForm extends \yii\base\Model
-{
-    public $username;
-    public $password;
-
-    public function rules()
-    {
-        return [
-            // define validation rules here
-        ];
-    }
-}
