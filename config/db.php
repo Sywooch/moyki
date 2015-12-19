e5f22838e79a0af175ab535473698cc00318caba
alex_azbukin <azbukin777@gmail.com>
2015-12-20 00:06:56 +0200

    change db config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=vehicle',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=moyki',
+    'username' => 'moyki_user',
+    'password' => 'moyki_password_123',
     'charset' => 'utf8',
 ];
